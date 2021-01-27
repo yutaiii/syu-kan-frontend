044b60d329403f5d90a23c163b7530bb78cc25bf
市川裕太 <yu74ichi69@gmail.com>
2021-01-28 00:10:04 +0900

    成果登録画面にstepperを導入

--- src/components/RoutineCheck.vue
+++ src/components/RoutineCheck.vue
@@ -1,24 +1,57 @@
 <template>
   <v-container>
-    <v-row justify="center" align-content="center">
-      <v-col cols="4"></v-col>
-      <v-col cols="4">
-        <template v-for="(r, index) in routines">
-          <v-checkbox
-            class="routine-checkbox"
-            :key="index"
-            :input-value="r.acheived"
-            :label="r.name"
-            @change="updateAcheived(index)"
-          ></v-checkbox>
-        </template>
-      </v-col>
-      <v-col cols="4"></v-col>
-    </v-row>
-    <v-btn
-      color="success"
-      @click="onRegisterClick()"
-    >登録</v-btn>
+    <v-stepper v-model="stepper">
+      <v-stepper-header>
+        <v-stepper-step
+          :complete="stepper > 1"
+          step="1"
+        >
+          今日の成果登録
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step
+          :complete="stepper > 2"
+          step="2"
+        >
+          登録完了
+        </v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-row justify="center" align-content="center">
+            <v-col cols="4"></v-col>
+            <v-col cols="4">
+              <template v-for="(r, index) in routines">
+                <v-checkbox
+                  class="routine-checkbox"
+                  :key="index"
+                  :input-value="r.acheived"
+                  :label="r.name"
+                  @change="updateAcheived(index)"
+                ></v-checkbox>
+              </template>
+            </v-col>
+            <v-col cols="4"></v-col>
+          </v-row>
+          <v-btn
+            color="success"
+            @click="onRegisterClick()"
+          >登録</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <div>登録完了。今日も1日お疲れ様でした！</div>
+          <v-btn
+            class="ma-3"
+            color="success"
+            to="/"
+          >ホームに移動</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </v-container>
 </template>
 
@@ -31,6 +64,7 @@ export default {
     return {
       routines: [],
       checkbox: true,
+      stepper: 1,
     }
   },methods: {
     updateAcheived(index) {
@@ -50,6 +84,7 @@ export default {
       // Post to API
       axios.post('http://localhost:8000/progress', requestParams)
         .then(res => {
+          this.stepper = 2;
           console.log('res', res)
         })
         .catch(e => {
