c9e2b19374a3bf26b10822e8239534c57f49c217
市川裕太 <yu74ichi69@gmail.com>
2021-01-28 08:43:08 +0900

    レイアウトを修正

--- src/components/RoutineCheck.vue
+++ src/components/RoutineCheck.vue
@@ -1,57 +1,68 @@
 <template>
   <v-container>
-    <v-stepper v-model="stepper">
-      <v-stepper-header>
-        <v-stepper-step
-          :complete="stepper > 1"
-          step="1"
-        >
-          今日の成果登録
-        </v-stepper-step>
+    <v-row justify="center" align-content="center">
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-stepper v-model="stepper">
+          <v-stepper-header>
+            <v-stepper-step
+              :complete="stepper > 1"
+              step="1"
+            >
+              今日の成果登録
+            </v-stepper-step>
 
-        <v-divider></v-divider>
+            <v-divider></v-divider>
 
-        <v-stepper-step
-          :complete="stepper > 2"
-          step="2"
-        >
-          登録完了
-        </v-stepper-step>
-      </v-stepper-header>
+            <v-stepper-step
+              :complete="stepper > 2"
+              step="2"
+            >
+              登録完了
+            </v-stepper-step>
+          </v-stepper-header>
 
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <v-row justify="center" align-content="center">
-            <v-col cols="4"></v-col>
-            <v-col cols="4">
-              <template v-for="(r, index) in routines">
-                <v-checkbox
-                  class="routine-checkbox"
-                  :key="index"
-                  :input-value="r.acheived"
-                  :label="r.name"
-                  @change="updateAcheived(index)"
-                ></v-checkbox>
-              </template>
-            </v-col>
-            <v-col cols="4"></v-col>
-          </v-row>
-          <v-btn
-            color="success"
-            @click="onRegisterClick()"
-          >登録</v-btn>
-        </v-stepper-content>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-row justify="center" align-content="center">
+                <v-col cols="12">
+                  <template v-for="(r, index) in routines">
+                    <v-checkbox
+                      class="routine-checkbox"
+                      :key="index"
+                      :input-value="r.acheived"
+                      :label="r.name"
+                      @change="updateAcheived(index)"
+                    ></v-checkbox>
+                  </template>
+                </v-col>
+              </v-row>
+              <v-btn
+                class="ma-3"
+                color="gray"
+                to="/"
+              >ホームに移動</v-btn>
+              <v-btn
+                class="ma-3"
+                color="success"
+                @click="onRegisterClick()"
+              >登録</v-btn>
+            </v-stepper-content>
 
-        <v-stepper-content step="2">
-          <div>登録完了。今日も1日お疲れ様でした！</div>
-          <v-btn
-            class="ma-3"
-            color="success"
-            to="/"
-          >ホームに移動</v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
+            <v-stepper-content step="2">
+              <div>登録完了。今日も1日お疲れ様でした！</div>
+              <v-btn
+                class="ma-3"
+                color="success"
+                to="/"
+              >ホームに移動</v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+
   </v-container>
 </template>
 
